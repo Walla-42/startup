8993a62b8b34a6cccc199e75b649bdfb8c548d52
Walla-42 <loganpwallace@gmail.com>
2025-10-07 13:50:30 -0600

    feat: added name that molecule html to the name_that_molecule.jsx + updated html formating errors

--- src/name_that_molecule/name_that_molecule.jsx
+++ src/name_that_molecule/name_that_molecule.jsx
@@ -2,16 +2,32 @@ import React from 'react';
 import './name_that_molecule.css'
 
 export function NameThatMolecule() { 
-    <main>
-        <div id="about-section">
-            <h2>
-                What is BioMatch
-            </h2>
-            <p>
-                BioMatch is a gamified quiz platform that transforms molecular biology into a fast-paced competition. 
-                Players race against the clock to identify amino acids, nucleotides, and sequences, with scores updated to a live leaderboard. 
-                It makes learning biology competitive and fun — perfect for students, educators, or biology enthusiasts looking to sharpen their knowledge through real-time challenges.
-            </p>
-        </div>
-    </main>
+    return (
+        <main class="game-layout">
+            <div class="game-scorecard">
+                <fieldset>
+                    <legend>Scorecard</legend>
+                    <p id="score">Score: 0</p>
+                    <p id="timer">Time Left: 30s</p>
+                    <p id="correct-answers">Correct: 0</p>
+                    <p id="incorrect-answers">Incorrect: 0</p>
+                </fieldset>
+            </div>
+
+            <div class="game-question">
+                <img id="question-image" class="game-question-image" src="https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/tryptophan/PNG" alt="question-image"/>
+
+                
+                <h3 id="question-text" class="game-question-text">What molecule is this?</h3>
+
+                <form class="game-form">
+                    <div id="answer-entry">
+                        <label for="answer">Answer: <input type="text" id="answer" name="answer" required/><br/></label>
+                    </div>
+                    <button type="button" class="button">Submit</button>
+                </form>
+            </div>
+
+        </main>
+    )
 }
