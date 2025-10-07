ba65b5295eb9fbbecc7274cb24e3e869b10c7cfc
Walla-42 <loganpwallace@gmail.com>
2025-10-07 17:32:23 -0600

    fix: fixed home page rendering and css styling dependancy

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -4,19 +4,19 @@ import './home.css'
 
 export function Home() { 
     return(
-    <main>
-            <div className="user-login-info">
-                <p>Logged in as: <span id="player-name">current-user</span></p>
-            </div>
-            <div className="GameButtons">
-                <NavLink to='/genome_trivia'><button className="game-button button">Genome Trivia</button></NavLink>
-                <NavLink to='/name_that_molecule'><button className="game-button button">Name That Molecule</button></NavLink>
-            </div>
+    <main className="home-main">
+        <div className="user-login-info">
+            <p>Logged in as: <span id="player-name">current-user</span></p>
+        </div>
+        
+        <div className="GameButtons">
+            <NavLink to='/genome_trivia'><button className="game-button button">Genome Trivia</button></NavLink>
+            <NavLink to='/name_that_molecule'><button className="game-button button">Name That Molecule</button></NavLink>
+        </div>
 
-            <div className="live-updates">
-                <p><span id="websocket-user">Walla42</span> has achieved a new highscore in <span id="websocket-game">Name That Molecule</span>!</p>
-            </div>
-
-        </main>
+        <div className="live-updates">
+            <p><span id="websocket-user">Walla42</span> has achieved a new highscore in <span id="websocket-game">Name That Molecule</span>!</p>
+        </div>
+    </main>
     )
 }
