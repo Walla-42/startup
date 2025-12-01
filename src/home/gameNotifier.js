0a593a1c1d371c44882a3caef454d60f1ab5bc55
Walla-42 <loganpwallace@gmail.com>
2025-11-30 17:11:54 -0700

    feat: fixed game constructor to broadcast events instead of utlizing examples

--- src/home/gameNotifier.js
+++ src/home/gameNotifier.js
@@ -15,51 +15,28 @@ class EventMessage {
 class GameEventNotifier {
   events = [];
   handlers = [];
-  simulatorIntervalId = null;
 
   constructor() {
-    if (!localStorage.getItem('gameNotifications')) {
-      localStorage.setItem('gameNotifications', JSON.stringify([]));
-    }
-  }
-
-  // NOTE: remove this when websocket is implemented
-  startSimulator() {
-    if (this.simulatorIntervalId !== null) {
-      return;
-    }
-
-    const gameArray = ['Name That Molecule', 'Genome Trivia'];
-    const userArray = ['Enoch', 'Walla42', 'AwesomeSauce', 'MoleculeMaster','FreakyLookinChicken'];
-    
-    this.simulatorIntervalId = setInterval(() => {
-      const score = Math.floor(Math.random() * 3000);
-      const date = new Date().toLocaleDateString();
-      const userName = userArray[Math.floor(Math.random() * userArray.length)];
-      const game = gameArray[Math.floor(Math.random() * gameArray.length)];
-      this.broadcastEvent(userName, GameEvent.End, { name: userName, score: score, date: date, game: game });
-    }, 5000);
-  }
-
-  // NOTE: remove this when websocket is implemented
-  stopSimulator() {
-    if (this.simulatorIntervalId !== null) {
-      clearInterval(this.simulatorIntervalId);
-      this.simulatorIntervalId = null;
-    }
+    let port = window.location.port;
+    const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+    this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+    this.socket.onopen = (event) => {
+      this.receiveEvent(new EventMessage('Simon', GameEvent.System, { msg: 'connected' }));
+    };
+    this.socket.onclose = (event) => {
+      this.receiveEvent(new EventMessage('Simon', GameEvent.System, { msg: 'disconnected' }));
+    };
+    this.socket.onmessage = async (msg) => {
+      try {
+        const event = JSON.parse(await msg.data.text());
+        this.receiveEvent(event);
+      } catch {}
+    };
   }
 
   broadcastEvent(from, type, value) {
     const event = new EventMessage(from, type, value);
-
-    const savedEvents = JSON.parse(localStorage.getItem('gameNotifications'));
-    savedEvents.unshift(event);
-    if (savedEvents.length > 9) {
-      savedEvents.splice(9);
-    }
-    localStorage.setItem('gameNotifications', JSON.stringify(savedEvents));
-
-    this.receiveEvent(event);
+    this.socket.send(JSON.stringify(event));
   }
 
   addHandler(handler) {
